9fee4d0812ed9c816072553e5e40ac02bcb26d38
utibe <inyangutibz@gmail.com>
2021-07-18 21:59:33 +0100

    adding new commits

---
+++ app.js
@@ -0,0 +1,22 @@
+const input = prompt('enter some numbers')
+const style = prompt('ascend or descend or reverse')
+
+//ascend
+if ((input != '') & (style == 'ascend')) {
+  var array = input.split('')
+  array.sort((a, b) => a - b)
+  alert(array.join(''))
+}
+//descend
+if ((input != '') & (style == 'descend')) {
+  var array = input.split('')
+  array.sort((a, b) => b - a)
+  alert(array.join(''))
+}
+//reverse
+if ((input != '') & (style == 'reverse')) {
+  var array = input.split('').reverse().join('')
+  alert(array.join(''))
+}
+//default
+else if (input == '' || style == '') alert('field cannot be left empty')
